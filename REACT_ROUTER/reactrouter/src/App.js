f2f255eacc7049864de17bf54a1ecf75433c5054
Mauregina <mauraregina@gmail.com>
2022-06-01 16:35:48 -0400

    add react router
    Signed-off-by: Mauregina <mauraregina@gmail.com>

--- REACT_ROUTER/reactrouter/src/App.js
+++ REACT_ROUTER/reactrouter/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 // components
 import Navbar from './components/Navbar';
@@ -12,6 +12,7 @@ import Product from './pages/Product'
 import About from './pages/About';
 import Info from './pages/Info';
 import NotFound from './pages/NotFound';
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -25,7 +26,12 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="/products/:id" element={<Product />}/>
           <Route path="/products/:id/info" element={<Info />}/>
-          <Route path="/about" element={<About />}/>
+          <Route path="/search" element={<Search/>} /> 
+
+          {/* Redirection */}
+          <Route path="/about" element={<About />} />
+          <Route path="/company" element={<Navigate to="/about" />} />
+
           <Route path="*" element={<NotFound />}/>
         </Routes>  
       </BrowserRouter>
--- REACT_ROUTER/reactrouter/src/components/SearchForm.js
+++ REACT_ROUTER/reactrouter/src/components/SearchForm.js
@@ -1,20 +1,23 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const SearchForm = () => {
-  const [query, setQuery] = useState("");
+  // Redireciona o usuario dentro do codigo do componente.
+  // Pega a busca do usuario e redireciona para pagina de busca
+  const navigate = useNavigate()
+  const [query, setQuery] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    navigate("/search?q=" + query);
   }
 
   return (
-    <div>
-        <form onSubmit={handleSubmit}>
-            <input type="text" onChange={(e)=>setQuery(e.target.value)}/>
-            <input type="submit" value="Buscar"/>
-        </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setQuery(e.target.value)}/>
+        <input type="submit" value="Buscar"/>
+    </form>
   )
 }
 
